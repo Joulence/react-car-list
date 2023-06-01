2b1010e87de749066fea03c767c945980135c54b
Joulence <4erviken@gmail.com>
2023-06-01 20:20:48 +0300

    Rewriten code for showing car description

--- src/components/CarsList.js
+++ src/components/CarsList.js
@@ -1,21 +1,39 @@
-
 import { DUMMY_DATA } from "../data/cars";
 import Card from "./UI/Card";
 import classes from "./CarsList.module.css";
+import { useState } from "react";
 
 const CarsList = () => {
+    const [cards, setCards] = useState(DUMMY_DATA); // Store the card data in state
+
+    const modalDescription = (cardId) => {
+        setCards((prevCards) => {
+            // Update the isShow value for the specific card that was clicked
+            return prevCards.map((card) => {
+                if (card.id === cardId) {
+                    return { ...card, isShow: !card.isShow };
+                }
+                return card;
+            });
+        });
+    };
+
     return (
         <div className={classes.carList}>
-            {DUMMY_DATA.map((item) => (
+            {cards.map((item) => (
                 <Card
                     key={item.id}
+                    id={item.id}
                     name={item.name}
                     link={item.link}
                     body={item.specifications.body}
                     engine={item.specifications.engine}
                     power={item.specifications.power}
-                    description={item.description}
-                />
+                    modalDescription={modalDescription}
+                    isShow={item.isShow} // Pass the isShow value for the specific card
+                >
+                    {item.description}
+                </Card>
             ))}
         </div>
     );
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,27 +1,30 @@
-import { Fragment } from 'react';
 import classes from './Card.module.css';
 
 const Card = (props) => {
+    const { id, name, link, body, engine, power, modalDescription, isShow } = props;
+
+    const handleClick = () => {
+        modalDescription(id); // Pass the card's ID to the modalDescription function
+    };
+
     return (
-        <Fragment>
-            <div className={classes.card}>
-                <h2 >{props.name}</h2>
-                <img src={props.link} alt={props.name} />
-                <div className={classes['card-spec']}>
-                    <div>Body</div>
-                    <div>{props.body}</div>
-                </div>
-                <div className={classes['card-spec']}>
-                    <div>Engine</div>
-                    <div>{props.engine}</div>
-                </div>
-                <div className={classes['card-spec']}>
-                    <div>Power</div>
-                    <div>{props.power}</div>
-                </div>
-                <p>{props.description}</p>
+        <div onClick={handleClick} className={classes.card}>
+            <h2>{name}</h2>
+            <img src={link} alt={name} />
+            <div className={classes['card-spec']}>
+                <div>Body</div>
+                <div>{body}</div>
             </div>
-        </Fragment>
+            <div className={classes['card-spec']}>
+                <div>Engine</div>
+                <div>{engine}</div>
+            </div>
+            <div className={classes['card-spec']}>
+                <div>Power</div>
+                <div>{power}</div>
+            </div>
+            {isShow && <p>{props.children}</p>}
+        </div>
     );
 }
 
