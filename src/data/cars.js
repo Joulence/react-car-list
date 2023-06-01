6c65071ee0160a53be833e190c66ff029f56b5d2
Joulence <4erviken@gmail.com>
2023-06-01 20:51:31 +0300

    Updated cars data

--- src/data/cars.js
+++ src/data/cars.js
@@ -6,7 +6,7 @@ export const DUMMY_DATA = [
         specifications: {
             body: "Roadster",
             engine: "2.7L H6 DOHC 24 valves",
-            power: "225 hp @ 6300 rpm"
+            power: "225 HP @ 6300 RPM"
         },
         description: "The Porsche Boxster is a mid-engine two-seater roadster. It was Porsche's first road vehicle to be originally designed as a roadster since the 914. The first-generation Boxster (the 986) was introduced in late 1996; it was powered by a 2.5-litre flat six-cylinder engine. The design was heavily influenced by the 1993 Boxster Concept. In 2000, the base model was upgraded to a 2.7-litre engine and a new Boxster S variant was introduced with a 3.2-litre engine. In 2003, styling and engine output was upgraded on both variants."
     },
@@ -17,8 +17,85 @@ export const DUMMY_DATA = [
         specifications: {
             body: "Roadster",
             engine: "5.7L V10 DOHC 40-valve",
-            power: "605 hp @ 8000 rpm"
+            power: "605 HP @ 8000 RPM"
         },
         description: "The Carrera GT is powered by a 5.7 L (5,733 cc) V10 engine rated at 450 kW (612 PS; 603 hp), whereas the original concept car featured a 5.5-litre version rated at 416 kW (566 PS; 558 hp). A road test in June 2004 by Car and Driver showed that the car can accelerate from 0–60 mph (97 km/h) in 3.5 seconds, 0–100 mph (161 km/h) in 6.8 seconds and 0–209 km/h (130 mph) in 10.8 seconds. The official top speed was 330 km/h (205 mph)."
+    },
+    {
+        id: 2,
+        name: "McLaren F1 GT 1997",
+        link: "https://static1.topspeedimages.com/wordpress/wp-content/uploads/jpg/201508/mclaren-f1-gt-16.jpg",
+        specifications: {
+            body: "Coupé ",
+            engine: "BMW S70/2 6,064 cc (370.0 cu in) V12 naturally-aspirated",
+            power: "27 HP @ 7500 RPM"
+        },
+        description: "The McLaren F1 GTR is the racing variant of the McLaren F1 sports car first produced in 1995 for grand touring style racing, such as the BPR Global GT Series, FIA GT Championship, JGTC, and British GT Championship. It was powered by the naturally aspirated BMW S70/2 V12 engine. It is most famous for its overall victory at the 1995 24 Hours of Le Mans where it won against faster purpose-built prototypes in very wet conditions. The F1 GTR raced internationally until 2005 when the final race chassis was retired."
+    },
+    {
+        id: 3,
+        name: "Porsche 918 Spyder 2014",
+        link: "https://upload.wikimedia.org/wikipedia/commons/2/29/Porsche_918_Spyder_at_2014_NY_Auto_Show.jpg",
+        specifications: {
+            body: "Roadster",
+            engine: "4.6-Liter V-8 W/ Two Electric Parallel Motors",
+            power: "599  HP @ 8,700  RPM"
+        },
+        description: "The 918 Spyder is a plug-in hybrid powered by a mid-mounted naturally aspirated 4.6 L (4,593 cc) V8 engine, developing 447 kW (608 PS; 599 hp) at 8,700 RPM, with two electric motors delivering an additional 210 kW (286 PS; 282 hp) for a combined output of 652 kW (875 hp) and 1,280 N⋅m (944 lbf⋅ft) of torque. The 918 Spyder's 6.8 kWh lithium-ion battery pack delivers an all-electric range of 19 km (12 mi) under the US Environmental Protection Agency's five-cycle tests."
+    },
+    {
+        id: 4,
+        name: "Aston Martin Vulcan AMP PRO 2017",
+        link: "https://media.astonmartin.com/wp-content/uploads/2018/07/839551.png",
+        specifications: {
+            body: "Coupé",
+            engine: "7.0-litre naturally-aspirated V12",
+            power: "811 HP @ 6000 RPM"
+        },
+        description: "The Aston Martin Vulcan is a two-door, two-seat, high-performance lightweight track-only car launched in 2015 by British luxury automobile manufacturer Aston Martin at the 2015 Geneva Motor Show. The Vulcan was designed by Aston Martin's creative officer Marek Reichman, taking inspiration from the then Aston Martin current models, such as the Vantage, the DB9 and the One-77. Production totalled 24 cars, with each priced at US$2.3 million. One was included with the penthouse of Aston Martin Residences tower in Miami."
+    },
+    {
+        id: 5,
+        name: "Bugatti Divo 2019",
+        link: "https://upload.wikimedia.org/wikipedia/commons/1/16/Bugatti_Divo%2C_GIMS_2019%2C_Le_Grand-Saconnex_%28GIMS0029%29.jpg",
+        specifications: {
+            body: "Coupé",
+            engine: "8.0L quad-turbocharged W16 unit",
+            power: "1,521  HP @ 6,700 RPM"
+        },
+        description: "Curtains up for the Bugatti Divo. At the exclusive automotive event 'The Quail: A Motorsports Gathering' in Monterey, California, Bugatti has presented its latest model as a world premiere. With the Divo, the French luxury brand has developed a super sports car tuned for agility, nimbleness and optimum handling performance on winding roads. It is powered by Bugatti's iconic eight-litre W16 engine with a power output of 1,500 PS. The aerodynamics of the model have been intensively fine-tuned and the suspension and chassis settings have been modified. As a result, the Bugatti Divo is 35 kilograms (77 lbs) lighter and has 90 kilograms (198 lbs) more downforce than the standard Chiron. The lateral acceleration of the Divo has been boosted to 1.6 g. Its maximum speed is limited to 380 km/h (236 mph). The Divo can lap the Nardò handling circuit in southern Italy eight seconds faster than the Chiron. The series will only consist of 40 vehicles. Upon the start of presentations to selected customers, the strictly limited small series, with a net unit price of €5 million, sold out immediately. It was therefore decided to build a super sports car with a different character from the Chiron which would still be immediately recognizable as a Bugatti.With the Divo, Bugatti is also reviving its coachbuilding tradition.In its first few decades, the French luxury brand had considerable success with bodies built to its own design and installed on existing chassis."
+    },
+    {
+        id: 6,
+        name: "Koenigsegg Jesko 2020",
+        link: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Koenigsegg_Jesko_at_Bridgehampton_C%26C%2C_front_right.jpg/1200px-Koenigsegg_Jesko_at_Bridgehampton_C%26C%2C_front_right.jpg",
+        specifications: {
+            body: "Coupé",
+            engine: "Koenigsegg twin turbo aluminum 5,0 L V8",
+            power: "1280  HP @ 8500 RPM"
+        },
+        description: "The Carrera GT is powered by a 5.7 L (5,733 cc) V10 engine rated at 450 kW (612 PS; 603 hp), whereas the original concept car featured a 5.5-litre version rated at 416 kW (566 PS; 558 hp). A road test in June 2004 by Car and Driver showed that the car can accelerate from 0–60 mph (97 km/h) in 3.5 seconds, 0–100 mph (161 km/h) in 6.8 seconds and 0–209 km/h (130 mph) in 10.8 seconds. The official top speed was 330 km/h (205 mph)."
+    },
+    {
+        id: 7,
+        name: "Mercedes-Benz AMG CLK GTR 1998",
+        link: "https://www.topgear.com/sites/default/files/2021/07/1998_Mercedes-Benz_AMG_CLK_GTR_002.jpg",
+        specifications: {
+            body: "Coupé ",
+            engine: "6.9L V12 6MT",
+            power: "612 HP @ 6800  RPM"
+        },
+        description: "In the '90s, Mercedes-Benz was eager to get back on the race track in style and aimed at the GT1 Class. But it had two problems: the McLaren F1 and the car built to defeat it, the Porsche 911 GT1. So to beat both of them, the three-pointed star brand secretly bought a racing McLaren F1, stripped it down, and the engineers learned how to build a better vehicle. The chassis was developed by Lola Composites in the U.K., while AMG prepared the engine. When the car was ready, it raced in the 1997 season and won the inaugural season of the FIA GT Championship with Bernd Schneider, and it did it again in the following year. But the world motorsports federation insisted on getting the production vehicles, so Mercedes-Benz commissioned AMG to build them in their manufacturing plant from Affalterbach."
+    },
+    {
+        id: 8,
+        name: "1998 TVR Cerebra Speed 12",
+        link: "https://upload.wikimedia.org/wikipedia/commons/e/e9/Cerbera_Speed_12.jpg",
+        specifications: {
+            body: "Coupé",
+            engine: "7.73L Speed 12 V12",
+            power: "800  HP @ 7250  RPM"
+        },
+        description: "With a curb weight of 2359 lbs (1070 kgs), the Cerbera Speed 12 has a naturally-aspirated V 12 cylinder engine, Petrol motor. This engine produces a maximum power of 811 PS (800 bhp - 596 kW) at 7250 rpm and a maximum torque of 881.0 Nm (649 lb.ft) at 7250 rpm. The power is transmitted to the road by the rear wheel drive (RWD) with a 6 speed Manual gearbox. On the topic of chassis details responsible for road holding, handling behavior and ride comfort, the Cerbera has Double wishbones. front suspension and Double wishbones. rear suspension. For stopping power, the Cerbera Speed 12 braking system includes Vented Discs at the front and Vented Discs at the rear. The Cerbera model is a Coupé car manufactured by Tvr, sold new from year 2000."
     },
 ]
