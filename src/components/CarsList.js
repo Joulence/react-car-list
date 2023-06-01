9081fd7f48f51a17356ae0454c44d96224a59727
Joulence <4erviken@gmail.com>
2023-06-01 22:11:45 +0300

    Fixed bugs

--- src/components/CarsList.js
+++ src/components/CarsList.js
@@ -1,94 +1,90 @@
-import { DUMMY_DATA } from "../data/cars";
 import Card from "./UI/Card";
 import classes from "./CarsList.module.css";
 import { useEffect, useState } from "react";
 
 const CarsList = () => {
-    const [cars, setCars] = useState([]);
-    const [cards, setCards] = useState(DUMMY_DATA);
-    const [isLoading, setIsLoading] = useState(true);
-    const [httpError, setHttpError] = useState();
+  const [cars, setCars] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+  const [openCardId, setOpenCardId] = useState(null); // Track the ID of the open card
 
-    useEffect(() => {
-        const fetchCars = async () => {
-            const response = await fetch('https://react-cars-list-default-rtdb.firebaseio.com/cars.json');
-
-            if (!response.ok) {
-                throw new Error('Error occured during fetching data');
-            }
-
-            const resData = await response.json();
-
-            const loadedCars = [];
-
-            for (const key in resData) {
-                console.log(resData[key].description);
-                loadedCars.push({
-                    id: key,
-                    name: resData[key].name,
-                    description: resData[key].description,
-                    link: resData[key].link,
-                    body: resData[key].specifications.body,
-                    engine: resData[key].specifications.engine,
-                    power: resData[key].specifications.power,
-                });
-            }
-            setCars(loadedCars);
-            setIsLoading(false);
-        };
-        fetchCars().catch((error) => {
-            setIsLoading(false);
-            setHttpError(error.message);
-        });
-    }, []);
-
-    if (isLoading) {
-        return (
-            <section>
-                <p>Loading...</p>
-            </section>
+  useEffect(() => {
+    const fetchCars = async () => {
+      try {
+        const response = await fetch(
+          "https://react-cars-list-default-rtdb.firebaseio.com/cars.json"
         );
-    }
 
-    if (httpError) {
-        return (
-            <section>
-                <p>{httpError}</p>
-            </section>
-        );
-    }
+        if (!response.ok) {
+          throw new Error("Error occurred during fetching data");
+        }
 
-    const modalDescription = (cardId) => {
-        setCards((prevCards) => {
-            // Update the isShow value for the specific card that was clicked
-            return prevCards.map((card) => {
-                if (card.id === cardId) {
-                    return { ...card, isShow: !card.isShow };
-                }
-                return card;
-            });
-        });
+        const resData = await response.json();
+
+        const loadedCars = [];
+
+        for (const key in resData) {
+          loadedCars.push({
+            id: key,
+            name: resData[key].name,
+            description: resData[key].description,
+            link: resData[key].link,
+            body: resData[key].specifications.body,
+            engine: resData[key].specifications.engine,
+            power: resData[key].specifications.power,
+          });
+        }
+
+        setCars(loadedCars);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setHttpError(error.message);
+      }
     };
 
+    fetchCars();
+  }, []);
+
+  const modalDescription = (cardId) => {
+    setOpenCardId((prevCardId) => (prevCardId === cardId ? null : cardId));
+  };
+
+  if (isLoading) {
     return (
-        <div className={classes.carList}>
-            {cars.map((car) => (
-                <Card
-                    key={car.id}
-                    id={car.id}
-                    name={car.name}
-                    link={car.link}
-                    body={car.body}
-                    engine={car.engine}
-                    power={car.power}
-                    modalDescription={modalDescription}
-                    isShow={car.isShow} // Pass the isShow value for the specific card
-                >
-                    {car.description}
-                </Card>
-            ))}
-        </div>
+      <section>
+        <p>Loading...</p>
+      </section>
     );
-}
+  }
+
+  if (httpError) {
+    return (
+      <section>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  return (
+    <div className={classes.carList}>
+      {cars.map((car) => (
+        <Card
+          key={car.id}
+          id={car.id}
+          name={car.name}
+          link={car.link}
+          body={car.body}
+          engine={car.engine}
+          power={car.power}
+          modalDescription={modalDescription}
+          isShow={openCardId === car.id} // Check if the current card ID matches the openCardId
+        >
+          {car.description}
+        </Card>
+      ))}
+    </div>
+  );
+};
 
 export default CarsList;
